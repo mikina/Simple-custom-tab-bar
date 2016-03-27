b5f3a4c1f1d4fa485c237f117b36337e7f7518ef
Mike Mikina <mike@mikina.pl>
2016-03-27 15:32:50 +0200

    Bug fix: Check if swipe back is not cancelled.

--- TabBarExample/SimpleCustomTabBar/SimpleCustomTabBar/RootViewController.swift
+++ TabBarExample/SimpleCustomTabBar/SimpleCustomTabBar/RootViewController.swift
@@ -15,6 +15,16 @@ class RootViewController: UIViewController, UINavigationControllerDelegate {
   }
   
   func navigationController(navigationController: UINavigationController, willShowViewController viewController: UIViewController, animated: Bool) {
+    // We send notification when transition is started, it's possible to cancel transition when
+    // user just start swipe back and cancel it. In this case tab bar will be shown when it's not suppose to.
+    // It's possible to check in transition coordinator if swipe back is not canceled.
+    if let coordinator = navigationController.topViewController?.transitionCoordinator() {
+      coordinator.notifyWhenInteractionEndsUsingBlock({ (context) in
+        if context.isCancelled() {
+          NSNotificationCenter.defaultCenter().postNotificationName(kPageHasStartedTransition, object: self, userInfo: nil)
+        }
+      })
+    }
     NSNotificationCenter.defaultCenter().postNotificationName(kPageHasStartedTransition, object: viewController, userInfo: nil)
   }
 }
